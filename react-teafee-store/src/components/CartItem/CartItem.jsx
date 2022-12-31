eb1a12b8b576c22726b839cbef046766a824c841
ashleykouch <ashleykouch99@gmail.com>
2022-12-31 13:06:54 +1100

    added remove item from cart

--- react-teafee-store/src/components/CartItem/CartItem.jsx
+++ react-teafee-store/src/components/CartItem/CartItem.jsx
@@ -1,23 +1,43 @@
+import { useEffect, useState } from "react";
 import styles from "./CartItem.module.scss";
 
 const CartItem = ({ carts }) => {
+  // function to delete product (unmount)
+
+  const [isMounted, setIsMounted] = useState(true);
+
+  const handleClick = () => {
+    setIsMounted(!isMounted);
+  };
+
+  useEffect(() => {
+    console.log(`isMounted has changed to ${isMounted}`);
+  }, [isMounted]);
+
   return (
     <>
       <span className={styles.LineBreak}>
         <hr />
       </span>
-      <div className={styles.Cart}>
-        <div className={styles.Cart_Image}>
-          <img src={carts.productImg} />
+      {isMounted ? (
+        <div className={styles.Cart}>
+          <div className={styles.Cart_Image}>
+            <img src={carts.productImg} />
+          </div>
+          <div className={styles.Cart_Items}>
+            <h3>{carts.productName}</h3>
+            <p>{carts.productBrand}</p>
+          </div>
+          <p>Quantity</p>
+          <h3>{carts.productPrice}</h3>
+          <button onClick={handleClick}>X</button>
         </div>
-        <div className={styles.Cart_Items}>
-          <h3>{carts.productName}</h3>
-          <p>{carts.productBrand}</p>
+      ) : (
+        <div className={styles.RemovedItem}>
+          <p>Item Removed</p>
         </div>
-        <p>Quantity</p>
-        <h3>{carts.productPrice}</h3>
-        <button>X</button>
-      </div>
+      )}
+      <div></div>
     </>
   );
 };
