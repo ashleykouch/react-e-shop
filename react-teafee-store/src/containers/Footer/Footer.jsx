42978e131d16e64ef20717f31b8b9bf5911067d9
ashleykouch <ashleykouch99@gmail.com>
2023-01-01 14:42:46 +1100

    finalised footer (excl. links)

--- react-teafee-store/src/containers/Footer/Footer.jsx
+++ react-teafee-store/src/containers/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Footer.module.scss";
+import email from "../../assets/email-logo.png";
+import github from "../../assets/github-logo.png";
+import linkedIn from "../../assets/linkedin-logo.png";
 
 const Footer = () => {
   return (
@@ -7,9 +10,18 @@ const Footer = () => {
       <div className={styles.Footer}>
         <div className={styles.Footer_Items}>
           <h2 className={styles.Footer_Items_Header}>Contact Info.</h2>
-          <p className={styles.Footer_Items_Content}>Github</p>
-          <p className={styles.Footer_Items_Content}>LinkedIn</p>
-          <p className={styles.Footer_Items_Content}>Email</p>
+          <div className={styles.Footer_Items_Img}>
+            <img src={github} alt="github" />
+            <p className={styles.Footer_Items_Content}>Github</p>
+          </div>
+          <div className={styles.Footer_Items_Img}>
+            <img src={linkedIn} alt="linkedin" />
+            <p className={styles.Footer_Items_Content}>LinkedIn</p>
+          </div>
+          <div className={styles.Footer_Items_Img}>
+            <img src={email} alt="email" />
+            <p className={styles.Footer_Items_Content}>Email</p>
+          </div>
         </div>
 
         <div className={styles.Footer_Items}>
@@ -31,8 +43,12 @@ const Footer = () => {
           <p className={styles.Footer_Items_Content}>
             Subscribe to receive updates, access to exclusive deals, and more
           </p>
-          <input type="text" placeholder="Enter your email address" />
-          <Link to="/">
+          <input
+            className={styles.Footer_Items_Input}
+            type="text"
+            placeholder="Enter your email address"
+          />
+          <Link to="/" className={styles.Footer_Items_Btn}>
             <button>Subscribe</button>
           </Link>
         </div>
